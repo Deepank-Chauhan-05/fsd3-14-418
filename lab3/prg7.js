a88e2c130bf32553416b4898c6539231740b828c
Deepank <chauhandeepankyg@gmail.com>
2026-08-31 09:46:52 +0530

    27 aug final commit

--- lab3/prg7.js
+++ lab3/prg7.js
@@ -2,11 +2,9 @@ import http from "http";
 import { readFile } from "fs/promises";
 
 const server = http.createServer(async (req, res) => {
-    const text = await readFile("big.txt");
-
-    res.end(text);
+  res.write("Loading....");
+  const text = await readFile("big.txt");
+  res.end(text);
 });
 
-server.listen(3000, () => {
-    console.log("Server is running...");
-});
+server.listen(3000, () => console.log("Server is running..."));
