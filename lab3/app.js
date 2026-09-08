68d0d92b0d4de6bf8b3206116a0452871cb32167
Deepank <chauhandeepankyg@gmail.com>
2026-09-08 10:32:39 +0530

    final commit 8 sep

--- lab3/app.js
+++ lab3/app.js
@@ -1,55 +1,57 @@
 import http from "http";
-
 // import * as teams from "teams.js";
-
 import { getAllTeams } from "./teams.js";
+import { addTeam } from "./teams.js";
+import { parse as parseUrl } from "url";
 
 const PORT = 5000;
-
 const sendJson = (res, statusCode, data) => {
-
   res.writeHead(statusCode, { "content-type": "application/json" });
-
   res.end(data === 'undefined' ? "" : JSON.stringify(data));
-
 };
 
 const parseJSONBody = (req) => {
-
   return new Promise((resolve, reject) => {
-
     let body = ''
-
     req.on("data", (chunk) => {
-
       body += chunk.toString()
-
     })
 
-    req.on("end", () =>{
-
-      try{
-
+    req.on("end", () => {
+      try {
         resolve(body ? JSON.parse(body) : {});
-
-      } catch (error){
-
+      } catch (error) {
         reject(error);
-
       }
+    });
+  });
+};
 
-    })
+const server = http.createServer(async (req, res) => {
+  const { pathname, query } = parseUrl(req.url, true);
+  const { method } = req;
+  console.log("pathname:", pathname);
+  console.log("query:", query);
+  console.log("Method:", method);
 
-  })
+  if (pathname === "/api/v1/teams" && method === 'GET') {
+    let teams = getAllTeams();
+    return sendJson(res, 200, teams);
+  }
 
-}
+  else if (pathname === "/api/v1/teams" && method === "POST") {
+    const newTeam = await parseJSONBody(req);
+    addTeam(newTeam);
+    return sendJson(res, 201, team);
+  }
 
-const server = http.createServer((req, res) => {
-
+  else {
+    res.statusCode = 404;
+    res.end();
+  }
+  res.end();
 });
 
 server.listen(PORT, () => {
-
   console.log("SIH Server is running at ", PORT);
-
 });
