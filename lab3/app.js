cd8d72e90c203aa6587af8a1bdb252d8b11ac882
Deepank <chauhandeepankyg@gmail.com>
2026-09-09 09:24:34 +0530

    update getTeams and addTeam

--- lab3/app.js
+++ lab3/app.js
@@ -1,22 +1,21 @@
 import http from "http";
 // import * as teams from "teams.js";
-import { getAllTeams } from "./teams.js";
-import { addTeam } from "./teams.js";
+import { getAllTeams, addTeam } from "./teams.js";
 import { parse as parseUrl } from "url";
 
 const PORT = 5000;
-const sendJson = (res, statusCode, data) => {
+
+const sendJson = (res, statusCode, data, keyword, msg) => {
   res.writeHead(statusCode, { "content-type": "application/json" });
-  res.end(data === 'undefined' ? "" : JSON.stringify(data));
+  res.end(data === "undefined" ? "" : JSON.stringify({ [keyword]: msg, data }));
 };
 
 const parseJSONBody = (req) => {
   return new Promise((resolve, reject) => {
-    let body = ''
+    let body = "";
     req.on("data", (chunk) => {
-      body += chunk.toString()
-    })
-
+      body += chunk.toString();
+    });
     req.on("end", () => {
       try {
         resolve(body ? JSON.parse(body) : {});
@@ -24,6 +23,7 @@ const parseJSONBody = (req) => {
         reject(error);
       }
     });
+    req.on("error", reject);
   });
 };
 
@@ -34,22 +34,22 @@ const server = http.createServer(async (req, res) => {
   console.log("query:", query);
   console.log("Method:", method);
 
-  if (pathname === "/api/v1/teams" && method === 'GET') {
+  if (pathname === "/api/v1/teams" && method === "GET") {
     let teams = getAllTeams();
-    return sendJson(res, 200, teams);
-  }
+    return sendJson(res, 200, teams, "count", teams.length);
+  } else if (pathname === "/api/v1/teams" && method == "POST") {
+    const { tname, tl, members } = await parseJSONBody(req);
+    if (!tname || !tl || !members)
+      return sendJson(400, {
+        error: "Team Name, Team Leader, or Members not defined",
+      });
+    const team = addTeam({ tname, tl, members });
 
-  else if (pathname === "/api/v1/teams" && method === "POST") {
-    const newTeam = await parseJSONBody(req);
-    addTeam(newTeam);
-    return sendJson(res, 201, team);
-  }
-
-  else {
+    return sendJson(res, 201, team, "Message", "Team registered successfully");
+  } else {
     res.statusCode = 404;
     res.end();
   }
-  res.end();
 });
 
 server.listen(PORT, () => {
--- lab3/teams.js
+++ lab3/teams.js
@@ -22,20 +22,20 @@ export const getAllTeams = () => teams;
 export const getTeamById = (id) => teams.find((team) => team.id === id);
 
 export const addTeam = (newTeam) => {
-    const team = { id: nextId++, newTeam };
+    const team = { id: nextId++, ...newTeam };
     teams.push(team);
     return team;
 };
 
-export const updateTeamById = (id, NewTeam) => {
+export const updateTeamById = (id, updateTeam) => {
     const team = getTeamById(id);
     if (!team) return null;
-    Object.assign(team, updateTeamById);
+    Object.assign(team, updateTeam);
     return team;
 };
 
 export const deleteTeam = (id) => {
-    const index = teams.findIndex((team) => team.id === id)
+    const index = teams.findIndex((team) => team.id === id);
     if (index == -1) return false;
     teams.splice(index, 1);
     return true;
